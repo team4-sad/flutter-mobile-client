ce064cf94b97a8cb0f091009451bc953d61f9a1d
Artemii Strukov <kavrese@gmail.com>
2025-11-17 16:02:40 +0300

    refactored schedule_choose_page.dart

---
+++ lib/features/schedule-choose/content/error_schedule_choose_content.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
+import 'package:miigaik/features/common/widgets/placeholder_widget.dart';
+import 'package:miigaik/features/schedule-choose/bloc/signature_schedule_bloc.dart';
+
+class ErrorScheduleChooseContent extends StatelessWidget {
+  final Object error;
+
+  ErrorScheduleChooseContent({super.key, required this.error});
+
+  final SignatureScheduleBloc bloc = GetIt.I.get();
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Center(
+      child: PlaceholderWidget.fromException(
+        error, () {
+          bloc.add(FetchSignaturesEvent());
+        }
+      ),
+    );
+  }
+
+}
--- lib/features/schedule-widget/schedule_widget_configuration_page.dart
+++ lib/features/schedule-widget/schedule_widget_configuration_page.dart
@@ -2,45 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:home_widget/home_widget.dart';
 
-class ScheduleWidgetConfigurationPage extends StatefulWidget {
+class ScheduleWidgetConfigurationPage extends StatelessWidget {
 
   final int widgetId;
 
-  const ScheduleWidgetConfigurationPage({super.key, required this.widgetId});
+  ScheduleWidgetConfigurationPage({super.key, required this.widgetId});
 
-  @override
-  State<ScheduleWidgetConfigurationPage> createState() => _ScheduleWidgetConfigurationPageState();
-}
-
-class _ScheduleWidgetConfigurationPageState extends State<ScheduleWidgetConfigurationPage> {
   final TextEditingController controller = TextEditingController();
-  bool _isLoading = false;
-
-  Future<void> save() async {
-    if (controller.text.isEmpty) return;
-
-    setState(() {
-      _isLoading = true;
-    });
-
-    try {
-      await HomeWidget.saveWidgetData(
-        '${widget.widgetId}_group',
-        controller.text,
-      );
-
-      await HomeWidget.updateWidget(name: 'ScheduleAppWidget');
-
-      await MethodChannel("widget_config").invokeMethod("finish");
-    } catch (e) {
-      debugPrint("Error saving widget: $e");
-      await MethodChannel("widget_config").invokeMethod("finish");
-    } finally {
-      setState(() {
-        _isLoading = false;
-      });
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -55,9 +23,7 @@ class _ScheduleWidgetConfigurationPageState extends State<ScheduleWidgetConfigur
               decoration: InputDecoration(labelText: "Введите группу"),
             ),
             const SizedBox(height: 20),
-            _isLoading
-                ? CircularProgressIndicator()
-                : ElevatedButton(
+            ElevatedButton(
               onPressed: save,
               child: Text("Сохранить"),
             )
@@ -65,5 +31,23 @@ class _ScheduleWidgetConfigurationPageState extends State<ScheduleWidgetConfigur
         ),
       ),
     );
+  }
+
+  Future<void> save() async {
+    if (controller.text.isEmpty) return;
+
+    try {
+      await HomeWidget.saveWidgetData(
+        '${widgetId}_group',
+        controller.text,
+      );
+
+      await HomeWidget.updateWidget(name: 'ScheduleAppWidget');
+
+      await MethodChannel("widget_config").invokeMethod("finish");
+    } catch (e) {
+      debugPrint("Error saving widget: $e");
+      await MethodChannel("widget_config").invokeMethod("finish");
+    }
   }
 }
